f6690e4d5f8a09fb5df23591638bfd649187c2d0
Vipin Kumar Rawat <rawat.vipin4818@gmail.com>
2023-02-08 18:14:56 +0530

    minor fix

--- pages/api/get-diet-plan.ts
+++ pages/api/get-diet-plan.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-// import { Configuration, OpenAIApi } from 'openai';
 
 type Data = {
   message: string;
@@ -9,18 +8,17 @@ type Data = {
   diet: unknown;
 };
 
-// const GPT_KEY = process.env.GPT_API_KEY;
-
-// const configuration = new Configuration({
-//   apiKey: GPT_KEY,
-// });
-
-// // const openai = new OpenAIApi(configuration);
+const GPT_KEY = process.env.GPT_API_KEY;
 
 // const headers = {
 //   'Content-Type': 'application/json',
 //   Authorization: `Bearer ${GPT_KEY}`,
 // };
+
+const headers = {
+  'Content-Type': 'application/json',
+  Authorization: `Bearer ${GPT_KEY}`,
+};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   let age = 18;
@@ -39,23 +37,26 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
   const basePrompt = `what is an ideal diet chart for ${age} year old ${gender} with height ${height} cm and weight ${weight} kgs for ${goal} based on meals in a day in form of markdown to be rendered in react?`;
   try {
-    // // const response = await openai.createCompletion({
-    // //   model: 'text-davinci-003',
-    // //   prompt: basePrompt,
-    // //   max_tokens: 550,
-    // //   temperature: 0,
-    // // });
-    // const diet = await response.data;
+    const response = await fetch('https://api.openai.com/v1/completions', {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt: basePrompt,
+        temperature: 0,
+        max_tokens: 550,
+      }),
+    });
+    const diet = await response.json();
 
-    // const pointsOfInterestPrompt = `Extract the points of interest out of this text, with no additional words, separated by commas: ${diet.choices[0].text}`;
+    const pointsOfInterestPrompt = `Extract the points of interest out of this text, with no additional words, separated by commas: ${diet.choices[0].text}`;
 
     res.status(200).json({
       message: 'success',
-      pointsOfInterestPrompt: basePrompt,
-      diet: 'Success',
+      pointsOfInterestPrompt,
+      diet: diet.choices[0].text,
     });
   } catch (err) {
     console.log('error: ', err);
-    // res.status(500).json({ message: 'failure', pointsOfInterestPrompt: '', diet: 'Failed' });
   }
 }
