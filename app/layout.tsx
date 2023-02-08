de8c49888973366989b384991c8294546bc76c98
Vipin Kumar Rawat <rawat.vipin4818@gmail.com>
2023-02-09 03:27:06 +0530

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,6 @@ import { AnalyticsWrapper } from './components/analytics';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
