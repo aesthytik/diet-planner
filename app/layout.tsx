ce2fdd9af8ed183fdaec0bd5f59910eb389be4b6
Vipin Kumar Rawat <rawat.vipin4818@gmail.com>
2023-02-09 02:16:19 +0530

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
-/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-undef */
 import './globals.css';
-import { DefaultSeo } from 'next-seo';
 import { AnalyticsWrapper } from './components/analytics';
-import SEO from '../next-seo.config';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -12,7 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <DefaultSeo {...SEO} />
+
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
@@ -20,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Diet Planner GPT" />
         <meta name="keywords" content="Diet planner, AI Diet Planner" />
+        <meta property="og:image" content="/banner.png" />
       </head>
       <body>
         <div className="bg-black">
