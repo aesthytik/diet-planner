9c906ba4a6dcd51f5e54a1833e5aaa75d10cc3c8
Vipin Kumar Rawat <rawat.vipin4818@gmail.com>
2023-02-08 19:28:01 +0530

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <title>Diet Planner GPT</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Diet Planner GPT" />
+        <meta name="keywords" content="Diet planner, AI Diet Planner" />
+      </head>
       <body>
         <div className="bg-black">
           {children}
